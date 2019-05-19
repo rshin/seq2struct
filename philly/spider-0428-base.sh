a2bccd37282acdb968f76164f0db62e80981db19
Alex Polozov <polozov@microsoft.com>
2019-05-19 13:50:06 -0700

    Initial Philly config for 0428-base

---
+++ philly/spider-0428-base.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+
+python3 train.py --config configs/spider-20190205/nl2code-0428-stability.philly.jsonnet --config-args "{bs: 50, lr: 1e-3, end_lr: 0, att: 0, data_path: '$PT_DATA_DIR'}" --logdir "$PT_OUTPUT_DIR/logdirs/20190428-stability"
