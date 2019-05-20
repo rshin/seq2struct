015c4faaa9885699d1539979aba036d8bf0edc99
Alex Polozov <polozov@microsoft.com>
2019-05-19 19:31:10 -0700

    Reopen the log file instead of flushing on Azure blobfuse

--- philly/spider-0428-base.sh
+++ philly/spider-0428-base.sh
@@ -4,4 +4,4 @@ source ./philly/setup.sh
 python3 train.py \
     --config configs/spider-20190205/nl2code-0428-stability.philly.jsonnet \
     --config-args "{bs: 50, lr: 1e-3, end_lr: 0, att: 0, data_path: '$PT_DATA_DIR/spider-20190205/'}" \
-    --logdir "$PT_OUTPUT_DIR/logdirs/20190428-stability/"
+    --logdir "$PT_OUTPUT_DIR/logdirs/20190519/"
