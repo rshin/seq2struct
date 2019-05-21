e7ab5ce88a689ff0011937a684529250a4b71259
Alex Polozov <polozov@microsoft.com>
2019-05-21 12:49:10 -0700

    Perform preprocessing on Philly

--- philly/setup.sh
+++ philly/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 pip install --user -e .
+sudo apt-get update
+sudo apt-get install -y default-jre
 export PT_OUTPUT_DIR="${PT_OUTPUT_DIR:-$PWD}"
 export CORENLP_HOME="$PT_DATA_DIR/third_party/stanford-corenlp-full-2018-10-05/"
+
+export LC_ALL="C.UTF-8"
+export LANG="C.UTF-8"
--- philly/spider-0518-idioms-sweep.sh
+++ philly/spider-0518-idioms-sweep.sh
@@ -6,6 +6,11 @@ parse_args "$@"
 set -o errexit -o pipefail -o noclobber -o xtrace
 
 source ./philly/setup.sh
+if [[ "$preprocess" = true ]]; then
+    python3 preprocess.py \
+        --config configs/spider-idioms/nl2code-0518.jsonnet \
+        --config-args "{bs: $bs, st: '$st', nt: $nt, lr: 2.5e-4, end_lr: 0, att: 0, data_path: '$PT_DATA_DIR/spider-20190205/'}"
+fi
 python3 train.py \
     --config configs/spider-idioms/nl2code-0518.jsonnet \
     --config-args "{bs: $bs, st: '$st', nt: $nt, lr: 2.5e-4, end_lr: 0, att: 0, data_path: '$PT_DATA_DIR/spider-20190205/'}" \
